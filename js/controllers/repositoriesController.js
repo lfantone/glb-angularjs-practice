05f7e4b38e7d21dacae08e352be42a182f5a51de
Marcos Diaz <marcos.ad88@gmail.com>
2015-06-01 14:05:24 -0300

    firts controller test

--- js/controllers/repositoriesController.js
+++ js/controllers/repositoriesController.js
@@ -6,11 +6,9 @@ app.controller("requestRepoController",["$scope","$routeParams","githubService",
         githubService.reposQuery(userGit)
         .then(
           function(result){
-                  console.log(result);
                   $scope.myvar=true;
                   $scope.vari=false;
                   $scope.repos = result;
-
           },
           function(err,status){
                   $scope.myvar=false;
